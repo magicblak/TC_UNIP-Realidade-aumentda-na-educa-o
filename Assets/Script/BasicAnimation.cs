b72e631ded654eb341a2b376f89c8e1754544922
Lucas Mathias <lucassantosmathias@gmail.com>
2021-07-07 19:19:06 -0300

    Ajuste de arquivos necessários Ajuste de scene inicial objeto para aplicação da fisica

--- Assets/Script/BasicAnimation.cs
+++ Assets/Script/BasicAnimation.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class BasicAnimation : MonoBehaviour
 {
+    public bool comeca = false;
+    public bool validation = true;
+    public Transform x;
+    Vector3 pos, posinicial;
     // Start is called before the first frame update
     void Start()
     {
-        
+        posinicial = x.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 1, 0.0f, Space.Self);
+        if (!comeca) return;
+        pos = x.position;
+        x.Rotate(0, 1, 0.0f, Space.Self);
+        if (x.position.y < -1)
+            validation = true;
+        else if (x.position.y > 0)
+            validation = false;
+        if (validation)
+            pos.y += 0.001f;
+        else
+            pos.y -= 0.001f;
+        x.position = pos;
     }
 }
