8ac68c5d528d20859884e6eedc00a501609cce69
Lucas Mathias <lucassantosmathias@gmail.com>
2021-07-07 22:11:41 -0300

    Adição de botão virtual que ativa fisica Adição de fisica em ambiente com esfera

--- Assets/Script/BasicAnimation.cs
+++ Assets/Script/BasicAnimation.cs
@@ -1,33 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
 public class BasicAnimation : MonoBehaviour
 {
-    public bool comeca = false;
-    public bool validation = true;
-    public Transform x;
-    Vector3 pos, posinicial;
+    public GameObject ball;
+    public VirtualButtonBehaviour button;
+    Vector3 posinicial;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        posinicial = x.position;
+        posinicial = ball.transform.position;
+        button.RegisterOnButtonPressed(onButtonPressed);
+        button.RegisterOnButtonReleased(onButtonRealesed);
+        rb = ball.GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+    void onButtonPressed(VirtualButtonBehaviour b)
+    {
+        Debug.Log("Pressiona");
+        rb.constraints = RigidbodyConstraints.None;
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    void onButtonRealesed(VirtualButtonBehaviour b)
+    {
+        Debug.Log("Não Pressiona");
+        rb.constraints = RigidbodyConstraints.FreezeAll;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!comeca) return;
-        pos = x.position;
-        x.Rotate(0, 1, 0.0f, Space.Self);
-        if (x.position.y < -1)
-            validation = true;
-        else if (x.position.y > 0)
-            validation = false;
-        if (validation)
-            pos.y += 0.001f;
-        else
-            pos.y -= 0.001f;
-        x.position = pos;
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        ball.transform.localPosition = posinicial;
     }
 }
