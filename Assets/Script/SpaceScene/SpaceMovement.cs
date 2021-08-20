2450ba5fcd5018aa64f72d8c93397261645c08ad
Lucas Mathias <lucassantosmathias@gmail.com>
2021-08-20 15:14:41 -0300

    Ajuste de movimentação translação Classe para movimento espacial reformulada Finalização da scnene de spaceAR

--- Assets/Script/SpaceScene/SpaceMovement.cs
+++ Assets/Script/SpaceScene/SpaceMovement.cs
@@ -5,69 +5,18 @@ using Vuforia;
 
 public class SpaceMovement : MonoBehaviour
 {
-    [SerializeField] private Transform planet;
-    [SerializeField] private GameObject explain_text, other_text;
-    [SerializeField] private VirtualButtonBehaviour button;
-    private bool controller;
-    [SerializeField] private int speed;
-    [SerializeField] private Vector3 rotation_direction;
-    [SerializeField] private int segment;
-    [SerializeField] private bool is_orbit;
-    private int current_segment;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        controller = false;
-        button.RegisterOnButtonPressed(onButtonPressed);
-        button.RegisterOnButtonReleased(onButtonRealesed);
-        explain_text.SetActive(false);
-        current_segment = 0;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(!is_orbit) rotationPlanet();
-        else orbitPlanet();
-    }
-
-    private void onButtonPressed(VirtualButtonBehaviour b)
-    {
-        controller = true;
-        explain_text.SetActive(true);
-        other_text.SetActive(false);
-    }
-
-    private void onButtonRealesed(VirtualButtonBehaviour b)
-    {
-        controller = false;
-        explain_text.SetActive(false);
-    }
-
-    private void orbitPlanet()
-    {
-        if (!controller) return;
-        planet.localPosition = calculeCircle(segment, current_segment);
-        current_segment++;
-        Debug.Log(current_segment);
-        if (current_segment > segment) current_segment = 0;
-    }
-
-    public static Vector3 calculeCircle(int segments, int current_segment)
+    public Vector3 calculeOrbit(int segments, int current_segment, float width, float height, float posz)
     {
         float posy, posx, angle;
-        float yaxis = 25, xaxis = 30;
         angle = ((float)current_segment / (float)segments) * 360 * Mathf.Deg2Rad;
-        posy = Mathf.Cos(angle) * yaxis;
-        posx = Mathf.Sin(angle) * xaxis;
-        return new Vector3(posx, posy, 9.49f);
+        posx = Mathf.Sin(angle) * width;
+        posy = Mathf.Cos(angle) * height;
+        return new Vector3(posx, posy, posz);
     }
 
-    private void rotationPlanet()
+    public void rotationPlanet(Transform planet, Vector3 rotation_direction, int speed)
     {
-        if (!controller) return;
         planet.Rotate(rotation_direction, speed * Time.deltaTime, Space.World);
     }
 }
