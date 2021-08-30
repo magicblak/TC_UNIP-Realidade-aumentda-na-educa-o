98d5823589c77f11b54fc189e93a0905560b1f77
Lucas Mathias <lucassantosmathias@gmail.com>
2021-08-30 16:52:18 -0300

    Ajustes e marcações para finalização da scene

--- Assets/Script/SkeletonScene/EmphasisController.cs
+++ Assets/Script/SkeletonScene/EmphasisController.cs
@@ -7,20 +7,32 @@ public class EmphasisController : MonoBehaviour
 {
     [SerializeField] private VirtualButtonBehaviour emphasis_buttom;
     [SerializeField] private GameObject emphasis_object;
+    [SerializeField] private TextMesh emphasis_text;
+    private static bool general_controller = false;
+    private bool local_controller;
     // Start is called before the first frame update
     void Start()
     {
+        local_controller = false;
         emphasis_buttom.RegisterOnButtonPressed(onEmphasisButtonPressed);
         emphasis_buttom.RegisterOnButtonReleased(onEmphasisButtonRealesed);
     }
 
     private void onEmphasisButtonPressed(VirtualButtonBehaviour b)
     {
+        if (EmphasisController.general_controller) return;
+        EmphasisController.general_controller = true;
+        local_controller = true;
         emphasis_object.SetActive(true);
+        emphasis_text.color = new Color32(255, 54, 54, 255);
     }
 
     private void onEmphasisButtonRealesed(VirtualButtonBehaviour b)
     {
+        if (!local_controller) return;
+        EmphasisController.general_controller = false;
+        local_controller = false;
         emphasis_object.SetActive(false);
+        emphasis_text.color = new Color32(255, 255, 255, 255);
     }
 }
