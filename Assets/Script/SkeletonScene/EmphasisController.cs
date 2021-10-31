349dc9304d9dbe91aef0c2bf40758893a9037d20
Lucas Mathias <lucassantosmathias@gmail.com>
2021-10-31 15:28:50 -0300

    - Classe para gamificação da cena - Botão que ativa gamificação - Perguntas e resultados

--- Assets/Script/SkeletonScene/EmphasisController.cs
+++ Assets/Script/SkeletonScene/EmphasisController.cs
@@ -1,3 +1,4 @@
+using Assets.Script.SkeletonScene;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,8 @@ public class EmphasisController : MonoBehaviour
     [SerializeField] private VirtualButtonBehaviour emphasis_buttom;
     [SerializeField] private GameObject emphasis_object;
     [SerializeField] private TextMesh emphasis_text;
+    [SerializeField] private TextMesh gamification_result_text;
+    [SerializeField] private GameObject gamification_result_bg;
     private static bool general_controller = false;
     private bool local_controller;
     // Start is called before the first frame update
@@ -20,12 +23,15 @@ public class EmphasisController : MonoBehaviour
 
     private void onEmphasisButtonPressed(VirtualButtonBehaviour b)
     {
-        Debug.Log(emphasis_text.text);
         if (EmphasisController.general_controller) return;
         EmphasisController.general_controller = true;
         local_controller = true;
-        emphasis_object.SetActive(true);
-        emphasis_text.color = new Color32(255, 54, 54, 255);
+        if (GamificationController.getIsGamification()) gamification();
+        else
+        {
+            emphasis_object.SetActive(true);
+            emphasis_text.color = new Color32(255, 54, 54, 255);
+        }
     }
 
     private void onEmphasisButtonRealesed(VirtualButtonBehaviour b)
@@ -35,5 +41,27 @@ public class EmphasisController : MonoBehaviour
         local_controller = false;
         emphasis_object.SetActive(false);
         emphasis_text.color = new Color32(255, 255, 255, 255);
+        gamification_result_text.text = "";
+        GamificationController.endGamification();
+        gamification_result_bg.SetActive(false);
+
+    }
+
+    private void gamification()
+    {
+        if (emphasis_object.activeSelf)
+        {
+            Debug.Log("Acertou");
+            gamification_result_bg.SetActive(true);
+            gamification_result_text.text = "VOCÊ ESTÁ NO CAMINHO CERTO, PARABÉNS!";
+            gamification_result_text.color = new Color32(87, 217, 162, 255);
+        }
+        else
+        {
+            Debug.Log("Errou");
+            gamification_result_bg.SetActive(true);
+            gamification_result_text.text = "QUE PENA ESTÁ ERRADO, MAS NÃO DESISTA!";
+            gamification_result_text.color = new Color32(224, 71, 75, 255);
+        }
     }
 }
---
+++ Assets/Script/SkeletonScene/GamificationController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Script.SkeletonScene
+{
+    class GamificationController : MonoBehaviour
+    {
+        [SerializeField] private Button gamification;
+        [SerializeField] private GameObject[] skeleton_pieces;
+        [SerializeField] private GameObject gamification_text;
+        [SerializeField] private Image gamification_image;
+        private static bool is_gamification = false;
+        private bool is_hidden = true;
+        // Start is called before the first frame update
+        private void Start()
+        {
+            gamification.onClick.AddListener(gamificationClick);
+        }
+
+        void gamificationClick()
+        {
+            is_gamification = !is_gamification;
+            if (is_gamification) prepareGamification();
+            else hideAllGamification();
+        }
+
+        private void prepareGamification()
+        {
+            gamification_image.color = new Color32(91, 217, 106, 255);
+            is_hidden = false;
+            int randon_index = Random.Range(0, 4);
+            gamification_text.SetActive(true);
+            skeleton_pieces[randon_index].SetActive(true);
+        }
+
+        private void hideAllGamification()
+        {
+            gamification_image.color = new Color32(212, 117, 117, 255);
+            is_hidden = true;
+            gamification_text.SetActive(false);
+            for (int i = 0; i < skeleton_pieces.Length; i++)
+            {
+                skeleton_pieces[i].SetActive(false);
+            }
+        }
+
+        public static bool getIsGamification()
+        {
+            return is_gamification;
+        }
+
+        public static void endGamification()
+        {
+            is_gamification = false;
+        }
+
+        private void Update()
+        {
+            if (!is_gamification && !is_hidden) hideAllGamification();
+        }
+    }
+}
