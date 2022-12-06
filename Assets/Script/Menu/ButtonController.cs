5b0d205823ed8002d775827c8b60b732691f50a3
Lucas Mathias <lucassantosmathias@gmail.com>
2022-12-06 16:50:09 -0300

    Ajuste e adaptação para entrega SENAC

--- Assets/Script/Menu/ButtonController.cs
+++ Assets/Script/Menu/ButtonController.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -7,6 +8,8 @@ public class ButtonController : MonoBehaviour
     [SerializeField] private GameObject button_obj;//Armazena o proprio objeto botão
     private Button button;//Armazena o componente button do objeto
     [SerializeField] public string name_obj;//Guarda o nome do objeto (nome da cena, botão e imagem)
+    [SerializeField] private string password = "";//Guarda o nome do objeto (nome da cena, botão e imagem)
+    [SerializeField] private InputField input_pass;
 
     //Executa sempre que a classe for instanciada na cena, similar a um construtor
     void Start()
@@ -18,6 +21,10 @@ public class ButtonController : MonoBehaviour
     //Ação do click
     private void clickButton()
     {
+        if (input_pass != null)
+        {
+            if (input_pass.text != password) return;
+        }
         //Carregar a cena com informações de ambiente
         PlayerPrefs.SetString("name", name_obj);//Salva na memoria uma variavel name com o valor do nome do objeto
         SceneManager.LoadScene(1, LoadSceneMode.Single);//Realiza o carregamento da cena RA
